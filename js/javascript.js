1f5a044de1355895801d1cc6ea5a5a8d5da70a36
Corey Fulop <corey.fulop@gmail.com>
2023-09-22 11:41:17 +0100

    Create very basic gameController, displayController, and playerOne/Two objects

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,17 @@ const gameBoard = (function () {
     return {gameBoardArray};
 })();
 
+const gameController = (function () {
+    return {};
+})();
+
+const displayController = (function () {
+    return {};
+})();
+
+function playerFactory(playerName) {
+    return {playerName};
+}
+
+const playerOne = playerFactory("playerOne");
+const playerTwo = playerFactory("playerTwo");
